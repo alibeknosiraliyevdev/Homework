4273c4dfebaf94a876a1ea5bc54a5a6e4d144a84
nosiraliyevalibek-web <nosiraliyevalibek.com>
2026-06-03 16:36:18 +0500

    1-vazifa

---
+++ lesson19.py
@@ -0,0 +1,19 @@
+# # 1. Car class yozish
+# class Car:
+#     def __init__(self , model, rang, turi, yoqilgi_turi):
+#         self.model=model
+#         self.rang=rang
+#         self.turi=turi
+#         self.yoqilgi_turi=yoqilgi_turi
+
+#     def info(self):
+#         print(self.model,self.rang,self.turi,self.yoqilgi_turi)
+
+# car1=Car("BMW", "Qora", "Mexanik", "Benzin")
+# car1.info()
+
+# 2. Teacher class yozish
+
+# 3. Animal → Cat inheritance qilish
+# 4. Getter/Setter qo‘shish
+# 5. Polymorphism misoli yozish
