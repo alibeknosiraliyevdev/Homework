6bd52a9107b29b8f5acd99c48176390d41fc82d1
nosiraliyevalibek-web <nosiraliyevalibek.com>
2026-06-03 17:31:19 +0500

    vazifa

--- lesson19.py
+++ lesson19.py
@@ -12,8 +12,71 @@
 # car1=Car("BMW", "Qora", "Mexanik", "Benzin")
 # car1.info()
 
-# 2. Teacher class yozish
+# # 2. Teacher class yozish
+# class Teacher:
+#     def __init__(self, name, age, fan_nomi, lavozimi):
+#         self.name=name
+#         self.age=age
+#         self.fan_nomi=fan_nomi
+#         self.lavozimi=lavozimi
+    
+#     def info(self):
+#         print(self.name,self.age ,self.fan_nomi, self.lavozimi)
 
-# 3. Animal → Cat inheritance qilish
-# 4. Getter/Setter qo‘shish
-# 5. Polymorphism misoli yozish
+# teacher1=Teacher("Xojiakbar", 34, "Elektronika va Sxemalari", "Dotsent")
+# teacher1.info()        
+
+# # 3. Animal → Cat inheritance qilish
+# class Animal:
+#     def __init__(self, name, zoti, ranggi):
+#         self.name=name
+#         self.zoti=zoti
+#         self.ranggi=ranggi
+
+# class Cat(Animal):
+#     def __init__(self, name, zoti, ranggi, ovozi):
+#         super().__init__(name, zoti, ranggi)
+#         self.ovozi=ovozi
+
+#     def info(self):
+#         print(f"Mushuk: {self.name}, {self.zoti}, {self.ranggi}, {self.ovozi}")
+    
+# cat1=Cat("Mosh" , "Shvetsariya mushugi", "Malla", "Meow")
+# cat1.info()
+
+# # 4. Getter/Setter qo‘shish
+# class Car:
+#     def __init__(self , model, rang, turi, yoqilgi_turi):
+#         self.model=model
+#         self.rang=rang
+#         self.turi=turi
+#         self.yoqilgi_turi=yoqilgi_turi
+
+#     def get_model(self):
+#         return self.model
+    
+#     def set_turi(self, new_turi):
+#         self.turi=new_turi   
+#         return self.turi
+
+# car1=Car("BMW", "Qora", "Mexanik", "Benzin")
+# print(car1.get_model())
+# print(car1.set_turi("Avtomat"))
+
+# # 5. Polymorphism misoli yozish
+# class Animal:
+#     def sound(self):
+#         print("Hayvon ovozi")
+
+# class Dog(Animal):
+#     def sound(self):
+#         print("Vov-vov")
+
+# class Cat(Animal):
+#     def sound(self):
+#         print("Miyov-miyov")
+
+# animals = [Dog(), Cat()]
+
+# for animal in animals:
+#     animal.sound()
